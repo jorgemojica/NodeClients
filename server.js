7456265503b7e6d45073e0b6ea3ceeac89cb8b50
jorgemojica <jorgemojica32@gmail.com>
2022-08-16 10:03:40 -0500

    Actualización archivo server.js

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const cors = require("cors");
 
 const app = express();
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: "http://localhost:3000"
 }
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/model");
 db.sequelize.sync();
